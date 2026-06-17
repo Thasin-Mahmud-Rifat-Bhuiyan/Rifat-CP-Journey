3a3aabb4cfa096f4b341f8d7513aa9e995b60fec
Thasin-Mahmud-Rifat-Bhuiyan <tmrifat03@gmail.com>
2026-06-18 03:47:47 +0600

    Add files via upload

---
+++ A_Way_Too_Long_Words.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int n;
+  cin >> n;
+  string str;
+  while (n--) {
+    cin >> str;
+    if (str.size() > 10) {
+      cout << str[0] << str.size() - 2 << str.back() << endl;
+    } else
+      cout << str << endl;
+  }
+
+  return 0;
+}
